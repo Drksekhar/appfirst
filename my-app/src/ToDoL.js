659ea168613695f1546cf6de04b651f7b92cc9c3
Drksekhar <ksekharabcd@gmail.com>
2023-11-26 17:22:37 +0300

    To List Local storage

--- my-app/src/ToDoL.js
+++ my-app/src/ToDoL.js
@@ -1,36 +1,55 @@
-import { useState } from 'react'
-import { Button, Input, List } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Input } from 'semantic-ui-react'
 
 export default function ToDoL() {
-    const [text, setText] = useState('')
-    const [item, setItem] = useState([])
-
-    function handleText(e) {
-        setText(e.target.value)
+    let oldData = []
+    if (localStorage.getItem('myData')) {
+        oldData = JSON.parse(localStorage.getItem('myData'))
     }
-
-    function addItem() {
-        if (Text === '') return
-        let newItem = item.concat(text)
-        setItem(newItem)
+    const [text, setText] = useState('')
+    const [list, setList] = useState(oldData)
+    function addItem(e) {
+        e.preventDefault()
+        if (text === ' ') return
+        const newList = list.concat(text)
+        setList(newList)
+        localStorage.setItem('myData', JSON.stringify(newList))
         setText('')
-
-
+    }
+    function deleteAll() {
+        const newList = []
+        setList(newList)
+        localStorage.setItem('myData', JSON.stringify(newList))
+    }
+    function deleteOne(index) {
+        let newList = [...list]
+        newList.splice(index, 1)
+        setList(newList)
+        localStorage.setItem('myData', JSON.stringify(newList))
     }
 
     return (
         <div>
-            <Input type='text' value={text} onChange={handleText} />
-            <Button color='blue' onClick={addItem}>ADD</Button>
-            <hr />
-            <h2>Things to do</h2>
-            <ol>
-                {
-                    item.map((item, index) => {
-                        return (<li key={index}>{item}</li>)
-                    })
-                }
-            </ol>
+            <form onSubmit={addItem}>
+                <Input
+                    type="text"
+                    value={text}
+
+                    onChange={(e) => { setText(e.target.value) }}
+                />
+                <Button color='blue'>ADD</Button>
+                <Button color='blue' onClick={deleteAll}   > Delete ALl</Button>
+                {list.map((item, index) => {
+                    return (
+                        <ol><li key={index}>
+                            {item}
+                            {/* Button to delete a specific item */}
+                            <button onClick={() => deleteOne(index)}>x</button>
+                        </li>
+                        </ol>
+                    );
+                })}
+            </form>
         </div>
     )
 }
